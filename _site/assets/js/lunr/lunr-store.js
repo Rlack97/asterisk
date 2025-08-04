31d78da3a9276b4dad1820354563f409aecedcff
Rlack97 <amalilith97@gmail.com>
2025-08-04 13:41:05 +0900

    deleted posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,12 @@ var store = [{
         "title": "Baz Boom Identity",
         "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
         "tags": [],
-        "url": "/portfolio/baz-boom-identity/",
-        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+        "url": "/asterisk/portfolio/baz-boom-identity/",
+        "teaser": "/asterisk/assets/images/unsplash-gallery-image-1-th.jpg"
       },{
-        "title": "Fizz Bang Identity",
-        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
-        "tags": [],
-        "url": "/portfolio/fizz-bang-identity/",
-        "teaser": "/assets/images/unsplash-gallery-image-2-th.jpg"
-      },{
-        "title": "Foo Bar Identity",
-        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
-        "tags": [],
-        "url": "/portfolio/foo-bar-website/",
-        "teaser": "/assets/images/foo-bar-identity-th.jpg"
-      },{
-        "title": "Ginger Gulp Identity",
-        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
-        "tags": [],
-        "url": "/portfolio/ginger-gulp-identity/",
-        "teaser": "/assets/images/unsplash-gallery-image-3-th.jpg"
-      },{
-        "title": "Hello, Jekyll!",
-        "excerpt":"안녕하세요!  이것은 Minimal Mistakes 테마에서 작성한 첫 번째 포스트입니다. 앞으로의 개발 지식 공유를 이 블로그에서 해 나가려고 합니다.   마크다운 지원      리스트   코드블록   이미지 등 다양한 마크다운 문법을 사용할 수 있습니다.~  ","categories": ["일상","개발"],
-        "tags": ["jekyll","minimal-mistakes","시작"],
-        "url": "/%EC%9D%BC%EC%83%81/%EA%B0%9C%EB%B0%9C/hello-world/",
-        "teaser": null
-      },{
-        "title": "이걸로 두 번째 실험",
-        "excerpt":"안녕하세요! 이것은 Minimal Mistakes 테마에서 작성한 두 번째 글입니다 마크다운도 그렇지만, const k = 32 let a = 28 result = k + a print(result) 마크다운 지원 마크다운 지원 문법을 잘 알아둬야겠죠? 리스트 코드블록 이미지 등 다양한 마크다운 문법을 사용할 수 있습니다.~ 이것도 리스트 되나? 이렇게 넣어도 되나? 이것도? 요것도?...","categories": ["일상","개발"],
-        "tags": ["jekyll","minimal-mistakes","시작"],
-        "url": "/%EC%9D%BC%EC%83%81/%EA%B0%9C%EB%B0%9C/%EB%91%90%EB%B2%88%EC%A7%B8%ED%8F%AC%EC%8A%A4%ED%8A%B8/",
+        "title": "거의 두 달만에 다시 손대보네요.",
+        "excerpt":"대체 왜 안되는건데?   마크다운으로 포스트를 작성하는건 좋다지만, 영 진척이 없네. 쉬운 프레임워크라고 했었던 거 같은데, 대체 왜 이럴까.   그래도 드디어 표시는 되니까, config부터 다시 해 보자구.  ","categories": ["개발"],
+        "tags": ["jekyll"],
+        "url": "/asterisk/%EA%B0%9C%EB%B0%9C/%EC%9E%AC%EC%8B%9C%EC%9E%91/",
         "teaser": null
       }]
