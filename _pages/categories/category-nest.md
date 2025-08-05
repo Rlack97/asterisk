199117b06a9ebb7fa2961a604b4bf3fb047f8859
Rlack97 <amalilith97@gmail.com>
2025-08-05 13:18:42 +0900

    0805

---
+++ _pages/categories/category-nest.md
@@ -0,0 +1,9 @@
+---
+title: "Nest.js"
+layout: archive
+permalink: /categories/nest
+author_profile: true
+--- 
+
+{% assign posts = site.categories.nest %}
+{% for post in posts %} {% include archive-single.html type=page.entries_layout %} {% endfor %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Jekyll 블로그 만들기",
+        "excerpt":"Jekyll 블로그란? Jekyll은 정적 사이트 생성기로, 마크다운 파일을 HTML로 변환해주는 도구입니다. 장점 빠른 로딩: 정적 파일이므로 매우 빠름 GitHub Pages 지원: 무료로 호스팅 가능 마크다운 지원: 간편한 글 작성 테마 다양성: 다양한 테마 사용 가능 설치 방법 # Ruby 설치 (macOS) brew install ruby # Jekyll 설치 gem install jekyll...","categories": ["개발"],
+        "tags": ["Jekyll","블로그","GitHub Pages"],
+        "url": "/asterisk/%EA%B0%9C%EB%B0%9C/Jekyll-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
+        "teaser": "/asterisk/assets/imgs/teaser.png"
+      },{
+        "title": "첫 번째 포스트",
+        "excerpt":"안녕하세요! 이것은 제 첫 번째 블로그 포스트입니다.   소개   이 블로그는 개발 관련 내용을 공유하기 위해 만들었습니다.   앞으로 다룰 주제들      웹 개발   프로그래밍 언어   개발 도구   프로젝트 후기   마무리   앞으로 많은 내용을 공유하겠습니다. 감사합니다!  ","categories": ["블로그","nest"],
+        "tags": ["첫포스트","Jekyll"],
+        "url": "/asterisk/%EB%B8%94%EB%A1%9C%EA%B7%B8/nest/%EC%B2%AB%EB%B2%88%EC%A7%B8-%ED%8F%AC%EC%8A%A4%ED%8A%B8/",
+        "teaser": "/asterisk/assets/imgs/teaser.png"
+      }]
